db41a8a535f900399d4c8b2e543cfedc4d583171
Shimous <pedroshimote@gmail.com>
2024-01-26 22:53:48 -0300

    Criado tratamento de erros para moedas que não possuem par em REAL como a UNI

--- main.py
+++ main.py
@@ -3,15 +3,15 @@
 #https://pypi.org/project/python-binance/
 #https://python-binance.readthedocs.io/en/latest/overview.html
 
+import os
+
+from binance.client import Client
+from datetime import datetime
 
 from secrets import api_secret, api_key, private_key_pass
 from subs_db import CONNECT, INSERT_INTO
 
-from binance.client import Client
-from datetime import datetime
-import os
-
-#Conexão com api Binance------------------------------------------------------------------------------------------------
+#Conexão com api Binance
 client = Client(api_key=api_key, api_secret=api_secret, private_key_pass=private_key_pass)
 url = "https://api.binance.com/api/v1/sua-requisicao"
 try:
@@ -32,15 +32,28 @@ for asset in conta['balances']:
     if float(quantidade) > 0:
         symbol_reais = f'{moeda}BRL'
         symbol_dollar = f'{moeda}FDUSD'
-        cotacao_reais = float(client.get_recent_trades(symbol=symbol_reais, limit=1)[0]['price'])
+        try:
+            cotacao_reais = float(client.get_recent_trades(symbol=symbol_reais, limit=1)[0]['price'])
+        except:
+            cotacao_reais = None
+
         try:
             cotacao_dollar = float(client.get_recent_trades(symbol=symbol_dollar, limit=1)[0]['price'])
         except:
-            symbol_dollar = f'FDUSD{moeda}'
-            cotacao_dollar = float(client.get_recent_trades(symbol=symbol_dollar, limit=1)[0]['price'])
+            try:
+                symbol_dollar = f'FDUSD{moeda}'
+                cotacao_dollar = float(client.get_recent_trades(symbol=symbol_dollar, limit=1)[0]['price'])
+            except:
+                symbol_dollar = f'{moeda}USDT'
+                cotacao_dollar = float(client.get_recent_trades(symbol=symbol_dollar, limit=1)[0]['price'])
         data_hora_requisicao = datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
-        saldo_reais = cotacao_reais * quantidade
+
+        try:
+            saldo_reais = cotacao_reais * quantidade
+        except TypeError as exc:
+            saldo_reais = 0
         saldo_dollar = cotacao_dollar * quantidade
+
 
         print(f'{moeda}: qtd = {quantidade}, Cotação atual R$ = {cotacao_reais}, Cotação atual $ = {cotacao_dollar} ')
         print(f'----> Saldo R$: {round(saldo_reais, 2)}, Saldo $: {round(saldo_dollar, 2)}')
@@ -50,7 +63,7 @@ for asset in conta['balances']:
         saldo_total_dollar = saldo_total_dollar + saldo_dollar
 
 print('==================================================')
-print(f'Saldo total R$: {round(saldo_total_reais,2)}, Saldo total $: {round(saldo_total_dollar, 2)}')
+print(f'Saldo total 11R$: {round(saldo_total_reais,2)}, Saldo total $: {round(saldo_total_dollar, 2)}')
 cotacao_dollar_real = float(client.get_recent_trades(symbol='USDTBRL', limit=1)[0]['price'])
 print(f'Cotação do Dollar: R${round(cotacao_dollar_real,2)}')
 
