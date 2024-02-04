a5dab2bacb3c7e403ab2e134453c9fe977815cce
Shimous <pedroshimote@gmail.com>
2024-02-04 12:42:19 -0300

    Alterada coluna Filters do arquivo lista_filters.csv com normalização dos valores para melhor vizualização dos dados.

--- subs_aux.py
+++ subs_aux.py
@@ -5,22 +5,24 @@ import pandas as pd
 
 from secrets import api_secret, api_key, private_key_pass
 
-#Conexão com api Binance
-client = Client(api_key=api_key, api_secret=api_secret, private_key_pass=private_key_pass)
-url = "https://api.binance.com/api/v1/sua-requisicao"
-try:
-    conta = client.get_account()
-except:
-    os.system('w32tm /resync')
-finally:
-    conta = client.get_account()
+#Salva informações de moedas e de trades
+def GetExchangeInfo():
+    client = Client(api_key=api_key, api_secret=api_secret, private_key_pass=private_key_pass)
+    try:
+        exchange_info = client.get_exchange_info()
+    except:
+        os.system('w32tm /resync')
+    finally:
+        exchange_info = client.get_exchange_info()
 
-# Obtendo informações sobre a exchange
-exchange_info = client.get_exchange_info()
+    # Obtendo informações sobre a exchange
+    exchange_info = client.get_exchange_info()
 
-# Exibindo os symbols disponíveis
-#for symbol_info in exchange_info['symbols']:
-#    print(symbol_info['symbol'])
+    lista_symbols = pd.DataFrame(exchange_info['symbols'])
+    lista_filters = pd.json_normalize(lista_symbols['filters'])
+    lista_symbols = pd.merge(lista_symbols, lista_filters, how='inner', left_index=True, right_index=True)
 
-lista_symbols = pd.DataFrame(exchange_info['symbols'])
-lista_symbols.to_csv('lista_symbols.csv', sep=';')
+    lista_symbols.to_csv('lista_symbols.csv', sep=';')
+
+if __name__ == '__main__':
+    GetExchangeInfo()
